3e52727a2bce1c57851ab7da09da5c73985943e2
jwillz7667 <jwillz7667@gmail.com>
2024-08-03 00:42:49 -0500

    feat: Add user profile update and order retrieval routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
+const { 
+  registerUser, 
+  loginUser, 
+  getUserProfile, 
+  updateUserProfile, 
+  getUserOrders 
+} = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/profile', protect, getUserProfile);
-
-module.exports = router;
-
-// Create a new file for supplier routes
-const supplierRouter = express.Router();
-const { registerSupplier, addProduct, getSupplierProducts } = require('../controllers/supplierController');
-
-supplierRouter.post('/register', protect, registerSupplier);
-supplierRouter.post('/products', protect, addProduct);
-supplierRouter.get('/products', protect, getSupplierProducts);
+router.route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router.get('/orders', protect, getUserOrders);
 
 module.exports = router;
